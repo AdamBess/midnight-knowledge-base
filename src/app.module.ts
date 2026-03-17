14553edc8e9c7d4702617f53bce39d583fc1bd4f
adam.bessisso <adam.bessisso@study.hs-duesseldorf.de>
2026-03-17 04:09:43 +0100

    feat: add PostgreSQL connection and database health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'postgres',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
